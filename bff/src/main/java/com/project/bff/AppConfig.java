dfe91ee731021983eb60ced6d46f503401f83592
Leandro Andrade <eusouleoandrade@gmail.com>
2024-08-07 02:26:55 -0300

    Configura o health check e atualiza a conexão com o banco de dados.

--- bff/src/main/java/com/project/bff/AppConfig.java
+++ bff/src/main/java/com/project/bff/AppConfig.java
@@ -1,34 +1,26 @@
 package com.project.bff;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    DataSource dataSource() {
+    // The database connection settings are in the properties
+    //
+    // @Bean
+    // DataSource dataSource() {
 
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    // DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/bffdb?useSSL=false&serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("pwdmysql");
+    // dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    // dataSource.setUrl("jdbc:mysql://localhost:3306/bffdb");
+    // dataSource.setUsername("root");
+    // dataSource.setPassword("pwdmysql");
 
-        return dataSource;
-    }
-
-    @Bean
-    JdbcTemplate jdbcTemplate(DataSource dataSource) {
-
-        return new JdbcTemplate(dataSource);
-    }
+    // return dataSource;
+    // }
 
     @Bean
     RestTemplate restTemplate() {
